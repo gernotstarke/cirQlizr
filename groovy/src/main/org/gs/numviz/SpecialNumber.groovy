cfe95983fa34b0ea8cd2c1e079425aea87e9bade
Dr. Gernot Starke <gs@gernotstarke.de>
2015-07-18 10:58:56 +0200

    interface for SpecialNumbers

---
+++ groovy/src/main/org/gs/numviz/SpecialNumber.groovy
@@ -0,0 +1,18 @@
+package org.gs.numviz
+
+// see end-of-file for license information
+
+
+interface SpecialNumber {
+    /**
+     * returns a specific digit of the special number.
+     * @param index position of the requested digit, starting with 0 (zero
+     * @return digit at location index
+     *
+     * Known subclasses: @see Pi, @see E
+     */
+    public int getDigit( int index)
+
+
+
+}
