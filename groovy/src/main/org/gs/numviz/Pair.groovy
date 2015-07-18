fe4267a4ca53a65e65187fce0c504e97fa5c2c82
Dr. Gernot Starke <gs@gernotstarke.de>
2015-07-18 23:07:48 +0200

    added Pair to have a decent datatype for the lines we need to draw

---
+++ groovy/src/main/org/gs/numviz/Pair.groovy
@@ -0,0 +1,22 @@
+package org.gs.numviz
+
+import groovy.transform.EqualsAndHashCode
+
+// see end-of-file for license information
+
+@EqualsAndHashCode
+class Pair {
+    int first
+    int second
+
+    public Pair( int first, int second) {
+        this.first = first
+        this.second = second
+    }
+
+    public String toString() {
+        return "("+first + ", " + second + ")"
+    }
+
+
+}
---
+++ groovy/src/test/org/gs/numviz/PairTest.groovy
@@ -0,0 +1,16 @@
+package org.gs.numviz
+
+// see end-of-file for license information
+
+
+class PairTest extends GroovyTestCase {
+
+    public void testEqualPair() {
+        def p1 = new Pair(3,1)
+        def p2 = new Pair(3,1)
+
+        assertEquals( "$p1 and $p2 should be equal", p1, p2)
+    }
+
+    
+}
