577aaa5679b33e9dc3c6170c60a761421e5b7ad7
Dr. Gernot Starke <gs@gernotstarke.de>
2015-07-18 10:59:33 +0200

    skeleton for Pi class(es)

---
+++ groovy/src/main/org/gs/numviz/Pi.groovy
@@ -0,0 +1,7 @@
+package org.gs.numviz
+
+// see end-of-file for license information
+
+
+class Pi implements SpecialNumber {
+}
---
+++ groovy/src/test/org/gs/numviz/PiTest.groovy
@@ -0,0 +1,14 @@
+package org.gs.numviz
+
+import org.junit.Test
+
+// see end-of-file for license information
+
+
+class PiTest extends GroovyTestCase {
+
+    @Test
+    public void firstDigitIsThree() {
+        
+    }
+}
