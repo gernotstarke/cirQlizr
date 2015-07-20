797d8f97da891a4fc36b37686117c612adb526d0
Dr. Gernot Starke <gs@gernotstarke.de>
2015-07-20 07:40:41 +0200

    formatting

--- groovy/src/main/org/gs/numviz/Pi.groovy
+++ groovy/src/main/org/gs/numviz/Pi.groovy
@@ -29,7 +29,7 @@ class Pi  {
              0,4,2,8,7,5,5,4,6,8,7,3,1,1,5,9,5,6,2,8,6,3,8,8,2,3,5,3,7,8,7,5,9,3,7,5,1,9,5,7,7,8,1,8,5,
              7,7,8,0,5,3,2,1,7,1,2,2,6,8,0,6,6,1,3,0,0,1,9,2,7,8,7,6,6,1,1,1,9,5,9,0,9,2,1,6,4,2,0,1,9,8
             ]
-    
+
     public static int getDigit( int position) {
         if (indexIsWithinBounds(position) ) {
             return digits.get(position) }
