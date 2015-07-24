94ac46c5494db3263efa4f583a02a7ffb98c1887
Dr. Gernot Starke <gs@gernotstarke.de>
2015-07-24 14:59:26 +0200

    added MIT licence to all classes

--- src/main/org/gs/numviz/NumVizColor.groovy
+++ src/main/org/gs/numviz/NumVizColor.groovy
@@ -13,7 +13,7 @@ class NumVizColor {
     //         Arrays.asList(0x67001F,0xb2182B,0xb2182B,0xb2182B, 0xf4a582, 0xfddbc7, 0xd1e5f0 )
 
     // scheme for diverging data
-    public final static List<Color> color = Arrays.asList(
+    public final static List<Color> divergingColor = Arrays.asList(
             new Color(158, 1, 66),
             new Color(213, 62, 79),
             new Color(244, 109, 67),
@@ -28,7 +28,7 @@ class NumVizColor {
 
     // scheme for qualitative data, more colors
     // see: http://colorbrewer2.org/?type=qualitative&scheme=Paired&n=10
-    public final static List<Color> color2 = Arrays.asList(
+    public final static List<Color> qualitativeColor = Arrays.asList(
             new Color(166, 206, 227),
             new Color(31, 120, 180),
             new Color(178, 223, 138),
@@ -39,4 +39,32 @@ class NumVizColor {
             new Color(255, 127, 0),
             new Color(202, 178, 214),
             new Color(106, 61, 154))
+
+
+    public static List<Color> color = qualitativeColor
 }
+
+/*********************************************************************************
+ The MIT License (MIT)
+
+ Copyright (c) 2015 Dr. Gernot Starke
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all
+ copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ SOFTWARE.
+
+ *********************************************************************************/
--- src/main/org/gs/numviz/Segment.groovy
+++ src/main/org/gs/numviz/Segment.groovy
@@ -1,47 +1,10 @@
 package org.gs.numviz
 
-import javax.swing.JFrame
-import java.awt.EventQueue
-
-// see end-of-file for license information
 
 
-class Application extends JFrame {
+class Segment {
 
-        final static int X_SIZE = 800
-        final static int Y_SIZE = 700
-
-
-        public Application() {
-            initUI();
-        }
-
-
-        private void initUI() {
-
-            add(new NumberGrapher( X_SIZE, Y_SIZE ));
-
-            setTitle("Number Visualizer");
-            setSize(X_SIZE, Y_SIZE);
-            setLocationRelativeTo(null);
-            setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-            setResizable( false ) // resolves issue #9
-        }
-
-
-        public static void main(String[] args) {
-
-            EventQueue.invokeLater(new Runnable() {
-
-                @Override
-                public void run() {
-                    Application ex = new Application(  );
-                    ex.setVisible(true);
-                }
-            });
-        }
-    }
+}
 
 /*********************************************************************************
  The MIT License (MIT)
@@ -67,5 +30,3 @@ class Application extends JFrame {
  SOFTWARE.
 
  *********************************************************************************/
-
-
