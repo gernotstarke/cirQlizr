12b5e61161fb978be4af0651a60dd7d6713eff46
Dr. Gernot Starke <gs@gernotstarke.de>
2015-07-24 11:19:30 +0200

    optimized placement of legend

--- src/main/org/gs/numviz/NumberGrapher.groovy
+++ src/main/org/gs/numviz/NumberGrapher.groovy
@@ -8,7 +8,9 @@ import java.awt.Graphics;
 import java.awt.Graphics2D;
 import javax.swing.JPanel
 import java.awt.Point
+import java.awt.Rectangle
 import java.awt.RenderingHints
+import java.awt.Shape
 import java.awt.font.TextAttribute
 import java.awt.geom.Arc2D
 import java.awt.geom.Ellipse2D
@@ -17,25 +19,25 @@ import java.awt.geom.Ellipse2D
 class NumberGrapher extends JPanel {
     final static String SELF_PRAISE = "Number visualizer, https://github.com/gernotstarke/num-viz  "
 
-    // the following are defined in NumberVisualizer
-    final Integer X_SIZE
-    final Integer Y_SIZE
+    // size of drawing canvas
+    final Integer X_CANVAS_SIZE
+    final Integer Y_CANVAS_SIZE
 
     private Point center
     private int radius
 
     // margin between (invisible) circle and JPanel border
-    final Integer MARGIN = 10
+    final Integer MARGIN = 20
 
 
-    public NumberGrapher( Integer xSize, Integer ySize ) {
+    public NumberGrapher(Integer xFrameSize, Integer yFrameSize) {
         super()
-        this.X_SIZE = xSize
-        this.Y_SIZE = ySize
+        this.X_CANVAS_SIZE = xFrameSize
+        this.Y_CANVAS_SIZE = yFrameSize - 40
 
         // crash when dimensions are too small
-        assert X_SIZE > 1
-        assert Y_SIZE > 1
+        assert X_CANVAS_SIZE > 1
+        assert Y_CANVAS_SIZE > 1
 
         this.center = center()
         this.radius = radius()
@@ -44,43 +46,72 @@ class NumberGrapher extends JPanel {
     /*
      * calculates the position of the center
      */
-    private Point center() {
-        int min = Math.min( X_SIZE, Y_SIZE)
-        int minHalf = min.intdiv( 2 )
 
-        return new Point( minHalf, minHalf )
+    private Point center() {
+        int min = Math.min(X_CANVAS_SIZE, Y_CANVAS_SIZE)
+        int minHalf = min.intdiv(2)
+
+        return new Point(minHalf, minHalf)
     }
 
     private radius() {
-        return (Math.min( X_SIZE, Y_SIZE).intdiv(2)) - MARGIN
+        return (Math.min(X_CANVAS_SIZE, Y_CANVAS_SIZE).intdiv(2)) - MARGIN
     }
 
 
-    private void drawSegments( Graphics2D g2d ) {
-
+    private void drawSegments(Graphics2D g2d) {
 
         g2d.setPaint(Color.lightGray)
-        g2d.setStroke( new BasicStroke( 0.5f ))
+        g2d.setStroke(new BasicStroke(0.5f))
 
         // Circle -> specialized Ellipse, coords give
         // upper left corner of enclosing rectangle
-        g2d.draw( new Ellipse2D.Double( MARGIN, MARGIN, 2*radius, 2*radius ))
+        g2d.draw(new Ellipse2D.Double(MARGIN, MARGIN, 2 * radius, 2 * radius))
 
-        g2d.setStroke( new BasicStroke( 12.0f ))
+        g2d.setStroke(new BasicStroke(12.0f))
         Arc2D arc2D = new Arc2D.Double()
 
         (0..9).each { digit ->
-            g2d.setPaint( Color.DARK_GRAY)
-            arc2D.setArcByCenter( center.x, center.y, radius, digit*36 + 3, 30, Arc2D.OPEN)
-
-            g2d.draw( arc2D )
+            g2d.setPaint(NumVizColor.color[digit])
+            arc2D.setArcByCenter(center.x, center.y, radius, digit * 36 + 3, 30, Arc2D.OPEN)
+            g2d.draw(arc2D)
         }
-
     }
 
 
+    private void drawLegend(Graphics2D g2d) {
 
-    private void someSelfPraise( Graphics2D g2d) {
+        // setup some nice font parameters for the legend
+        initLegendFont(g2d)
+
+
+        (0..9).each { digit ->
+            // set color scheme
+            g2d.setPaint( NumVizColor.color[digit])
+
+            // draw filled rectangle
+            g2d.fillRect(X_CANVAS_SIZE - 60, Y_CANVAS_SIZE - (digit + 1) * 35, 30, 30)
+
+            // show corresponding digit
+            g2d.drawString( digit.toString(), X_CANVAS_SIZE-22, Y_CANVAS_SIZE-digit*35-14)
+        }
+    }
+
+
+    private void initLegendFont(Graphics2D g2d) {
+        Font font = new Font(Font.SERIF, Font.TRUETYPE_FONT, 15);
+        Hashtable<TextAttribute, Object> map =
+                new Hashtable<TextAttribute, Object>();
+
+        /* This colour applies just to the font, not other rendering */
+        map.put(TextAttribute.FOREGROUND, Color.DARK_GRAY);
+
+        font = font.deriveFont(map);
+        g2d.setFont(font);
+    }
+
+
+    private void someSelfPraise(Graphics2D g2d) {
 
         // Graphics2D g2d = (Graphics2D) g;
 
@@ -107,13 +138,12 @@ class NumberGrapher extends JPanel {
         font = font.deriveFont(map);
         g2d.setFont(font);
 
-        FontMetrics fm = getFontMetrics( font );
-        int width = fm.stringWidth( SELF_PRAISE );
+        FontMetrics fm = getFontMetrics(font);
+        int width = fm.stringWidth(SELF_PRAISE);
 
-        g2d.drawString(SELF_PRAISE, Application.X_SIZE-width, Application.Y_SIZE-25);
+        g2d.drawString(SELF_PRAISE, Application.X_SIZE - width, Application.Y_SIZE - 25);
 
     }
-
 
 
     @Override
@@ -123,7 +153,9 @@ class NumberGrapher extends JPanel {
 
         Graphics2D g2d = (Graphics2D) g;
 
-        drawSegments( g2d )
+        drawSegments(g2d)
+
+        drawLegend( g2d )
 
         someSelfPraise(g2d)
 
