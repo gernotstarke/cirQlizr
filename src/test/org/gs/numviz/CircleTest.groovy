d9ccaaa3f6e32c2ddb1aef174c6530ab5b384169
Dr. Gernot Starke <gs@gernotstarke.de>
2015-07-25 16:55:14 +0200

    added a Circle class (#16) and some tests (not finished)

---
+++ src/test/org/gs/numviz/CircleTest.groovy
@@ -0,0 +1,37 @@
+package org.gs.numviz
+
+import org.junit.Before
+import org.junit.Test
+
+import java.awt.Point
+
+// see end-of-file for license information
+
+
+class CircleTest extends GroovyTestCase {
+
+    Circle circle
+
+    @Before
+    public void setUp() {
+        circle = new Circle( center: new Point(0,0), radius: 1)
+    }
+
+    /**
+    * on circle with radius 1 and center 0,0, the point x=1, y=0 should
+     * be on the circle!
+     */
+    @Test
+    public void testAngleZeroOnXAxis() {
+        circle = new Circle( center: new Point(0,0), radius: 1)
+
+        Point onXAxis = circle.getPointByAngle( 0 )
+
+        Double actualX = onXAxis.getX()
+        Double actualY = onXAxis.getY()
+        Double epsilon = 0.01
+
+        assertEquals( 1, actualX, epsilon )
+        assertEquals( 0, actualY, epsilon )
+    }
+}
