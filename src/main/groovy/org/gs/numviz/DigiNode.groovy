08035bab84b88346e9f8732f39ef0c7370020620
Dr. Gernot Starke <gs@gernotstarke.de>
2015-08-03 21:09:15 +0200

    moved code to Coordinate2D-class

--- src/main/groovy/org/gs/numviz/DigiNode.groovy
+++ src/main/groovy/org/gs/numviz/DigiNode.groovy
@@ -1,5 +1,8 @@
 package org.gs.numviz
 
+import java.awt.Point
+import java.awt.geom.Point2D
+
 // see end-of-file for license information
 
 /**
@@ -12,4 +15,28 @@ class DigiNode {
     Coordinate2D coordinate
 
     double angle
+    double radius
+
+    /**
+     * completely create a digiNode instance with its coordinates
+     * @param angle
+     * @param radius
+     */
+    public DigiNode( double angle, double radius) {
+        this.angle = 0
+        this.radius = radius
+
+        this.coordinate = calcCoordinate( angle, radius)
+
+    }
+
+
+    public Coordinate2D calcCoordinate( double angle, double radius) {
+
+        return Circle.getPointByCenterRadiusAngle( new Coordinate2D(0,0), radius, angle )
+
+    }
+
+
+
 }
