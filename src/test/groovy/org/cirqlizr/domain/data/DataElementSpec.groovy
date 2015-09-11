d8baad963be0c1d30056d04237cbd59219a86e17
gernots <gernot.starke@innoq.com>
2015-09-11 06:55:30 +0200

    can I instantiate DataElements with various input types?

---
+++ src/test/groovy/org/cirqlizr/domain/data/DataElementSpec.groovy
@@ -0,0 +1,55 @@
+package org.cirqlizr.domain.data
+
+import spock.lang.Specification
+
+class DataElementSpec extends Specification {
+    DataElement dataElement
+
+    def "can instantiate DataElement with int"() {
+
+        int dozen = 12
+        when:
+            dataElement = new DataElement( dozen )
+
+        then:
+            dataElement.getValue() == dozen.toString()
+    }
+
+    def "can instantiate DataElement with String"() {
+        String the = "the"
+        when:
+            dataElement = new DataElement( the )
+        then:
+            dataElement.getValue() == the
+    }
+
+    def "can instantiate DataElement with Integer"() {
+        Integer i = 12
+        when: dataElement = new DataElement( i )
+        then: dataElement.getValue() == i.toString()
+
+    }
+
+
+}
+
+/************************************************************************
+ * This is free software - without ANY guarantee!
+ *
+ *
+ * Copyright Dr. Gernot Starke, arc42.org
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ *********************************************************************** */
+
