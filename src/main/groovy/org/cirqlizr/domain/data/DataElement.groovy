034358ccc43c088055feee680d5a56e4df3dac6d
gernots <gernot.starke@innoq.com>
2015-09-11 06:54:34 +0200

    override equals

--- src/main/groovy/org/cirqlizr/domain/data/DataElement.groovy
+++ src/main/groovy/org/cirqlizr/domain/data/DataElement.groovy
@@ -4,6 +4,27 @@ package org.cirqlizr.domain.data
 class DataElement {
     final String  value
 
+    public DataElement( int numericValue ) {
+        this.value = numericValue.toString()
+    }
+
+    public DataElement( String stringValue) {
+        this.value = stringValue
+    }
+
+    public DataElement( Integer intValue ) {
+        this.value = intValue.toString()
+    }
+
+
+    @Override
+    public boolean equals(Object o){
+        if ((o == null) ||
+           (!(o instanceof org.cirqlizr.domain.data.DataElement))) return false
+
+        DataElement de = (DataElement) o
+        return this.value == de.value
+    }
 }
 
 
